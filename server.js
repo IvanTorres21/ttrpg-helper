07fe7961e6c296ed527af44e6c1d18127da9d3be
Archsteel <ivantorresoses2@gmail.com>
2023-11-01 23:33:29 +0100

    added youtube playback capability

--- server.js
+++ server.js
@@ -12,18 +12,35 @@ const port = 7777 ;
 app.use(express.static(__dirname +'/www'))
 
 
-
+var lastCommand;
+var lastValue;
 
 io.on('connection', (socket) => {
   console.log('a user connected');
+
+  switch(lastCommand) {
+    case "audio":
+      break;
+      case "youtube":
+        break;
+  }
+
   socket.on('sendImage', data => {
     console.log(data);
     socket.broadcast.emit('changeImage', data)
     });
   socket.on('sendAudio', data => {
       console.log(data);
+      lastCommand = "audio";
+      lastValue = data;
       socket.broadcast.emit('playAudio', data);
   })
+  socket.on('sendUrl', data => {
+    console.log(data);
+    lastCommand = "youtube";
+    lastValue = data;
+    socket.broadcast.emit('sendUrl', data);
+})
   socket.on('pauseResume', data => {
       socket.broadcast.emit('pauseResume');
   })
