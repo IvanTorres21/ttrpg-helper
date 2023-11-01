971f3ec5790dc8d54a6a1a5f089e9ae8bc57329d
Archsteel <ivantorresoses2@gmail.com>
2023-11-01 23:01:44 +0100

    controller now hears music too

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-
+const port = 7777 ;
 
 
 
@@ -31,6 +31,6 @@ io.on('connection', (socket) => {
 
 
 
-server.listen(3000, () => {
-  console.log('listening on *:3000');
+server.listen(port, () => {
+  console.log('listening on *:' + port);
 });
